69effc501142b7db2c15b228f9eb5addb2adb2e6
Nsisong Akpan <sandersakpan@gmail.com>
2022-09-30 13:31:38 +0100

    fixed bank update bug

--- pages/user_dashboard/settings/index.vue
+++ pages/user_dashboard/settings/index.vue
@@ -6,10 +6,7 @@
         <div class="settings_wrap subscriber">
           <div class="update__password__wrap py-2">
             <div class="edit__wrap text-right">
-              <button
-                class="text-right"
-                @click="profile_modal = !profile_modal"
-              >
+              <button class="text-right" @click="profile_modal = !profile_modal">
                 Edit Profile
               </button>
             </div>
@@ -19,49 +16,27 @@
               <form action="" method="post" @submit.prevent="edit_profile()">
                 <div class="form-group">
                   <label class="" for="">Email</label>
-                  <input
-                    required
-                    type="text"
-                    class="form-control"
-                    v-model="update_profile.email"
-                    placeholder="Enter New Email"
-                  />
+                  <input required type="text" class="form-control" v-model="update_profile.email"
+                    placeholder="Enter New Email" />
                 </div>
                 <div class="form-group">
                   <label class="" for="">Phone Number</label>
-                  <input
-                    required
-                    type="number"
-                    class="form-control"
-                    v-model="update_profile.phone_number"
-                    placeholder="Enter New Phone Number"
-                  />
+                  <input required type="number" class="form-control" v-model="update_profile.phone_number"
+                    placeholder="Enter New Phone Number" />
                 </div>
 
                 <div class="d-flex justify-content-center">
                   <div class="mx-2">
-                    <v-btn
-                      class="cancel_button"
-                      @click="profile_modal = !profile_modal"
-                      >Cancel</v-btn
-                    >
+                    <v-btn class="cancel_button" @click="profile_modal = !profile_modal">Cancel</v-btn>
                   </div>
                   <div class="mx-2">
-                    <v-btn
-                      class="login_button"
-                      :loading="loading"
-                      value="Update"
-                      type="submit"
-                      >Update</v-btn
-                    >
+                    <v-btn class="login_button" :loading="loading" value="Update" type="submit">Update</v-btn>
                   </div>
                 </div>
               </form>
             </div>
           </div>
-          <div
-            class="update__password__wrap d-flex justify-content-between py-2"
-          >
+          <div class="update__password__wrap d-flex justify-content-between py-2">
             <div class="text-wrap">
               <h5>Change Password</h5>
               <p>Set a new password</p>
@@ -75,59 +50,32 @@
             <div class="password__modal slideInDown">
               <form action="" method="post" @submit.prevent="change_password()">
                 <div class="form-group">
-                  <div
-                    class="d-flex justify-content-between align-items-center"
-                  >
+                  <div class="d-flex justify-content-between align-items-center">
                     <div class="password__header">
                       <h6>Edit New Password</h6>
                     </div>
                   </div>
 
                   <label class="" for="">Enter Old Password</label>
-                  <input
-                    required
-                    type="password"
-                    class="form-control"
-                    v-model="password_change.current_password"
-                    placeholder="Enter Current Password"
-                  />
+                  <input required type="password" class="form-control" v-model="password_change.current_password"
+                    placeholder="Enter Current Password" />
                 </div>
                 <div class="form-group">
                   <label class="" for="">Enter New Password</label>
-                  <input
-                    required
-                    type="password"
-                    class="form-control"
-                    v-model="password_change.password"
-                    placeholder="Enter New Password"
-                  />
+                  <input required type="password" class="form-control" v-model="password_change.password"
+                    placeholder="Enter New Password" />
                 </div>
                 <div class="form-group">
                   <label class="" for="">Confirm New Password</label>
-                  <input
-                    required
-                    type="password"
-                    class="form-control"
-                    v-model="password_change.password_confirmation"
-                    placeholder="Confirm Password"
-                  />
+                  <input required type="password" class="form-control" v-model="password_change.password_confirmation"
+                    placeholder="Confirm Password" />
                 </div>
                 <div class="d-flex justify-content-center">
                   <div class="mx-2">
-                    <v-btn
-                      class="cancel_button"
-                      @click="show_modal = !show_modal"
-                      >Cancel</v-btn
-                    >
+                    <v-btn class="cancel_button" @click="show_modal = !show_modal">Cancel</v-btn>
                   </div>
                   <div class="mx-2">
-                    <v-btn
-                      class="login_button"
-                      :loading="loading"
-                      value="Update"
-                      type="submit"
-                      >Update</v-btn
-                    >
+                    <v-btn class="login_button" :loading="loading" value="Update" type="submit">Update</v-btn>
                   </div>
                 </div>
               </form>
@@ -138,59 +86,32 @@
             <div class="password__modal slideInDown">
               <form action="" method="post" @submit.prevent="updateBankDetails()">
                 <div class="form-group">
-                  <div
-                    class="d-flex justify-content-between align-items-center"
-                  >
+                  <div class="d-flex justify-content-between align-items-center">
                     <div class="password__header">
                       <h6>Enter Bank Details</h6>
                     </div>
                   </div>
 
                   <label class="" for="">Enter Bvn</label>
-                  <input
-                    required
-                    type="number"
-                    class="form-control"
-                    v-model="verification.bvn_number"
-                    placeholder="Enter your BVN"
-                  />
+                  <input required type="number" class="form-control" v-model="verification.bvn_number"
+                    placeholder="Enter your BVN" />
                 </div>
                 <div class="form-group">
                   <label class="" for="">Enter Bank Name</label>
-                  <input
-                    required
-                    type="text"
-                    class="form-control"
-                    v-model="verification.bank"
-                    placeholder="Enter Bank Name"
-                  />
+                  <input required type="text" class="form-control" v-model="verification.bank"
+                    placeholder="Enter Bank Name" />
                 </div>
                 <div class="form-group">
                   <label class="" for="">Enter Account Number</label>
-                  <input
-                    required
-                    type="number"
-                    class="form-control"
-                    v-model="verification.account_number"
-                    placeholder="Enter Account Number"
-                  />
+                  <input required type="number" class="form-control" v-model="verification.account_number"
+                    placeholder="Enter Account Number" />
                 </div>
                 <div class="d-flex justify-content-center">
                   <div class="mx-2">
-                    <v-btn
-                      class="cancel_button"
-                      @click="show_payment_modal = !show_payment_modal"
-                      >Cancel</v-btn
-                    >
+                    <v-btn class="cancel_button" @click="show_payment_modal = !show_payment_modal">Cancel</v-btn>
                   </div>
                   <div class="mx-2">
-                    <v-btn
-                      class="login_button"
-                      :loading="loading"
-                      value="Update"
-                      type="submit"
-                      >Update</v-btn
-                    >
+                    <v-btn class="login_button" :loading="loading" value="Update" type="submit">Update</v-btn>
                   </div>
                 </div>
               </form>
@@ -206,9 +127,7 @@
             <button>Edit</button>
           </div>
         </div> -->
-          <div
-            class="update__password__wrap d-flex justify-content-between py-2"
-          >
+          <div class="update__password__wrap d-flex justify-content-between py-2">
             <div class="text-wrap">
               <h5>Payments</h5>
               <p>Update Bank Details</p>
@@ -284,10 +203,13 @@ export default {
         this.$toast.success("BVN Verification Completed Successfully", {
           timeout: 5000,
         });
-        this.show_payment_modal = false;
+        this.show_payment_modal = !this.show_payment_modal;
         this.verification = {}
         console.log(response);
       } catch (error) {
+        this.$toast.warning("Bvn or Bank Details may be incorrect", {
+          timeout: 5000,
+        });
         console.log(error.response);
       }
     },
@@ -319,21 +241,26 @@ export default {
   height: 100%;
   padding: 0 50px;
 }
+
 .settings__gen__wrap {
   padding-top: 50px;
 }
+
 .settings_wrap {
   border-radius: 10px;
   background-color: #f8f7ff;
   padding: 10px;
 }
+
 .settings_wrap h5 {
   font-weight: 600;
   font-size: 15px;
 }
+
 .settings_wrap p {
   font-size: 15px;
 }
+
 .edit__wrap button {
   background-color: #fff;
   border: 1px solid #00e8fe;
@@ -341,6 +268,7 @@ export default {
   border-radius: 5px;
   padding: 5px 10px;
 }
+
 .kyc_link {
   background-color: #fff;
   border: 1px solid #00e8fe;
@@ -349,6 +277,7 @@ export default {
   padding: 5px 10px;
   color: #000;
 }
+
 /* .update__password__wrap {
 } */
 /* .settings_wrap .v-slide-group__content {
@@ -361,24 +290,19 @@ export default {
 .settings_wrap .v-slide-group {
   flex-wrap: wrap;
 }
-.settings_wrap .theme--light.v-tabs > .v-tabs-bar .v-tab:not(.v-tab--active),
-.settings_wrap
-  .theme--light.v-tabs
-  > .v-tabs-bar
-  .v-tab:not(.v-tab--active)
-  > .v-icon,
-.settings_wrap
-  .theme--light.v-tabs
-  > .v-tabs-bar
-  .v-tab:not(.v-tab--active)
-  > .v-btn,
-.settings_wrap .theme--light.v-tabs > .v-tabs-bar .v-tab--disabled {
+
+.settings_wrap .theme--light.v-tabs>.v-tabs-bar .v-tab:not(.v-tab--active),
+.settings_wrap .theme--light.v-tabs>.v-tabs-bar .v-tab:not(.v-tab--active)>.v-icon,
+.settings_wrap .theme--light.v-tabs>.v-tabs-bar .v-tab:not(.v-tab--active)>.v-btn,
+.settings_wrap .theme--light.v-tabs>.v-tabs-bar .v-tab--disabled {
   color: #55174f;
   font-variant: normal;
 }
+
 .settings_wrap .v-tab {
   text-transform: unset;
 }
+
 .change__password__form {
   background-color: rgba(0, 0, 0, 0.445);
   position: fixed;
@@ -388,18 +312,22 @@ export default {
   width: 100%;
   z-index: 999;
 }
+
 .change__password__form .form-control {
   color: grey;
   /* min-height: 7vh; */
   padding: 10px;
   font-size: 13px;
 }
+
 .change__password__form label {
   font-size: 13px;
 }
+
 .password__header h6 {
   font-weight: 700;
 }
+
 .password__modal {
   width: 20%;
   margin: 100px auto;
@@ -407,11 +335,13 @@ export default {
   padding: 30px 10px;
   border-radius: 3px;
 }
+
 .change__password__form p {
   cursor: pointer;
   font-size: 20px;
   font-weight: 700;
 }
+
 .change__password__form .login_button {
   background-color: #00e8fe !important;
   font-size: 14px;
@@ -421,6 +351,7 @@ export default {
   text-transform: none;
   /* width: 100%; */
 }
+
 .change__password__form .cancel_button {
   border: 1px solid #00e8fe;
   color: #00e8fe;
@@ -429,6 +360,7 @@ export default {
   box-shadow: none !important;
   text-transform: none;
 }
+
 .slideInDown {
   -webkit-animation-name: slideInDown;
   animation-name: slideInDown;
@@ -437,23 +369,27 @@ export default {
   -webkit-animation-fill-mode: both;
   animation-fill-mode: both;
 }
+
 @-webkit-keyframes slideInDown {
   0% {
     -webkit-transform: translateY(-100%);
     transform: translateY(-100%);
     visibility: visible;
   }
+
   100% {
     -webkit-transform: translateY(0);
     transform: translateY(0);
   }
 }
+
 @keyframes slideInDown {
   0% {
     -webkit-transform: translateY(-100%);
     transform: translateY(-100%);
     visibility: visible;
   }
+
   100% {
     -webkit-transform: translateY(0);
     transform: translateY(0);
@@ -465,22 +401,27 @@ export default {
     margin-left: 0 !important;
     padding: 0 !important;
   }
+
   .settings_wrap {
     margin: 0;
   }
+
   .change__password__form {
     padding: 10px;
   }
+
   .password__modal {
     width: 100%;
     margin: 60px auto;
   }
+
   .update__password__wrap h5,
   .update__password__wrap p,
   .edit__wrap button,
   .kyc_link {
     font-size: 13px;
   }
+
   .settings .setting__nav {
     position: fixed;
     background-color: #fff;
@@ -490,6 +431,7 @@ export default {
     box-shadow: 0px 4px 4px rgba(0, 232, 254, 0.15) !important;
     z-index: 1000;
   }
+
   .settings__gen__wrap {
     padding: 90px 15px;
   }
